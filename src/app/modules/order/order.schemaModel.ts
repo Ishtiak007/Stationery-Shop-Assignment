9fabd296251d85a1d736ec6abae457290c78779a
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-24 03:01:48 +0600

    Created a schema for the Order model

---
+++ src/app/modules/order/order.schemaModel.ts
@@ -0,0 +1,40 @@
+import mongoose, { model, Schema } from 'mongoose';
+import { IOrder } from './order.interface';
+
+// Create a schema for the Order model
+const OrderSchema: Schema = new Schema<IOrder>(
+  {
+    email: {
+      type: String, // Store the email of the customer
+      required: [true, 'Email is required for purchasing product'],
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, // Regular expression to validate email format
+        'Please enter a valid email',
+      ],
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'StationeryProductModel',
+      required: [true, 'Product is required'],
+    },
+    quantity: {
+      type: Number,
+      required: [true, 'Product Quantity is required'],
+      min: [1, 'Quantity must be at least 1'],
+    },
+    totalPrice: {
+      type: Number,
+      required: [true, 'Product total price is required'],
+      min: [0, "Total price can't be negative"],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+// Create and export the Order model based on the schema
+const OrderModel = model<IOrder>('Orders', OrderSchema);
+
+export default OrderModel;
