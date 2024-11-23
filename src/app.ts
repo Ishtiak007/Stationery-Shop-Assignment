db89d81acddb3fff97d34314f857d3bb2f6df5f6
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-23 21:35:12 +0600

    file created

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello developers 👻!');
+  res.send('Hello developers, I am Ishtiak From Rangpur 👻!');
 });
 
 export default app;
